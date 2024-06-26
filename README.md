71909d7643f553ddae39d47bd26171ff4511cd79
Darren Morley <64443172+GraphicBytes@users.noreply.github.com>
2024-06-26 12:59:27 +0100

    BitBucket migration

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# apache-docker-template
-This is a docker container for hosting static files that don't require PHP or a database.
+# APACHE ONLY DOCKER COMPOSE TEMPLATE # #
+
+### What is this repository for? ###
+
+This is a docker container for hosting a static build or pre-built react app that doesn't require PHP or a database.
+
+### Set up ###
+
+**Edit the .env file and update the variables to suit your project.**
+
+* COMPOSE_PROJECT_NAME=**[git repo name]**
+* HOST_NAME=**[project's production domain]**
+* HOST_CONTAINER_NAME=**[git repo name]**
+* HOST_CONTAINER_IMAGE=httpd:**[git repo name]**
+* HOST_PORT=**[Assign and use a port number used with an Nginx Reverse Proxy ]**
+
+### Project files ###
+
+Place project files inside the **"www"** directory.
+
+### scraps.txt ###
+
+This is a useful copy and paste scratch pad to use while developing
+
+### Server side boot up ###
+
+**Development and production**
+
+Via the command line, goto the root directory of the repo and use the command **"sh tool.sh"** to build or rebuild the docker container, this will also update the environment.
+
+### Nginx reverse proxy, SSL and DNS setup ###
+
+This docker container was intended to be used on shared hosting via Nginx Reverse proxy. Use Nginx to handle SSL and DNS pointing to the container's assigned port number
---
+++ tools.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+while true; do
+  clear
+  echo "############################"
+  echo " "
+  echo "      THE BODACH SHOW"
+  echo "        SHELL TOOLS"
+  echo " "
+  echo "############################"
+  echo " "
+  branchName=$(git rev-parse --abbrev-ref HEAD)
+  echo "Current Branch: $branchName"
+  echo " "
+  echo "Select an option:"
+  echo " "
+  echo "1. Cancel/Close"
+  echo "2. Start/Reboot docker container"
+  echo "3. Git pull changes"
+  echo "4. Git push changes to current branch"
+  echo "5. Shut down docker container"
+  echo " "
+  read -p "Enter option (1 to 5): " option
+
+  case $option in
+  1)
+    echo " "
+    echo "Operation cancelled. Bye Ó_ó"
+    echo " "
+    break
+    ;;
+  2)
+    echo " "
+    read -p "Launch docker container? Confirm [y/n]: " confirmation
+    if [ "$confirmation" = "y" ]; then
+      docker-compose --env-file .env down
+      docker-compose --env-file .env up -d --build
+    else
+      echo "Operation cancelled."
+    fi
+    ;;
+  3)
+    echo " "
+    echo "Pull all changes for this branch?"
+    read -p "Confirm [y/n]: " confirmation
+    if [ "$confirmation" = "y" ]; then
+      git reset --hard
+      git pull
+    else
+      echo "Operation cancelled."
+    fi
+    ;;
+  4)
+    echo " "
+    echo "Push changes to git?"
+    read -p "Confirm [y/n]: " confirmation
+    if [ "$confirmation" = "y" ]; then
+      echo "Select an option for your commit message:"
+      echo "1. Use default message ('Primary Dev Update')"
+      echo "2. Enter a custom message"
+      read -p "Enter option (1 or 2): " msgOption
+
+      if [ "$msgOption" = "1" ]; then
+        commitMessage="Primary Dev Update"
+      elif [ "$msgOption" = "2" ]; then
+        read -p "Enter your custom commit message: " customMessage
+        commitMessage="$customMessage"
+      else
+        echo "Invalid option selected. Exiting."
+        break
+      fi
+
+      git add --all
+      git commit -m "$commitMessage"
+      git push
+    else
+      echo "Operation cancelled."
+    fi
+    ;;
+  5)
+    echo " "
+    read -p "Shut down container? Confirm [y/n]: " confirmation
+    if [ "$confirmation" = "y" ]; then
+      if [[ "$branchName" == "master" || "$branchName" == "production" ]]; then
+        docker-compose --env-file .env down
+      else
+        echo " "
+        echo "Must be in one of the 3 main git branches (master or production)."
+        echo " "
+      fi
+    else
+      echo "Operation cancelled."
+    fi
+    ;;
+  *)
+    echo "Invalid option selected. Exiting."
+    ;;
+  esac
+done
